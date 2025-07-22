72304e7ecee01b6ca5dc45f110c7782d6bd47325
funfun <alpha5179@naver.com>
2025-07-22 14:03:22 +0900

    feat: s3 이미지 업로드 기능 구현

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import userrouter from './routers/userRouter';
 import reviewsRouter from './routers/reviewsRouter';
 import inquiriesRouter from './routers/inquiriesRouter';
 import multer from 'multer';
+import s3Router from './routers/s3Router';
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/users', userrouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
+app.use('/api/s3', s3Router);
 
 app.use(defaultNotFoundHandler);
 app.use(globalErrorHandler);
